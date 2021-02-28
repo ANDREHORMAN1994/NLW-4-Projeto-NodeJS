bd7a1ecbbd8467fe447cb1bc51f56bdd9b652516
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2021-02-28 07:36:34 -0300

    Iniciando minha aplicaçao e baixando dependencias - 28/02/2021

---
+++ API/src/server.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+
+const app = express();
+
+/*
+MÉTODOS DE REQUISIÇÃO HTTP:
+GET   -   BUSCAR INFORMAÇÃO NO SERVIDOR;
+POST  -   SALVAR UMA INFORMAÇÃO NO APP;
+PUT   -   ALTERAR ALGUMA INFORMAÇÃO;
+DELETE -  DELETAR ALGUMA INFORMAÇÃO;
+PATCH  -  ALTERAÇÃO ESPECÍFICA;
+
+SINTAXE:
+  1 PARAM => "ROTA";
+  2 PARAM => (request, response) => {};
+*/
+
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'HELLO WORLD, NLW#4',
+  })
+})
+
+app.post('/', (request, response) => {
+  // O método POST recebe os dados há serem salvos pelo REQUEST;
+  response.json({
+    message: 'DADOS SALVOS COM SUCESSO',
+  })
+})
+
+// Pode usar um LISTEN para testar o retorno do servidor funcionando via npm run dev;
+app.listen(7777, () => console.log("rodando"));
