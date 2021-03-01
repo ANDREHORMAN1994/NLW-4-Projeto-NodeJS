3edf8834ed69a17da7d8ffc7a2cec77c6fc35dd7
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2021-03-01 16:02:41 -0300

    Criando models e routes - 01/03/2021

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
+
+// Pode usar um LISTEN para testar o retorno do servidor funcionando via npm run dev;
+app.listen(7777, () => console.log("FUNCIONANDO"));
 
 /*
 MÉTODOS DE REQUISIÇÃO HTTP:
@@ -13,8 +22,7 @@ PATCH  -  ALTERAÇÃO ESPECÍFICA;
 SINTAXE:
   1 PARAM => "ROTA";
   2 PARAM => (request, response) => {};
-*/
-
+  
 app.get('/', (request, response) => {
   return response.json({
     message: 'HELLO WORLD, NLW#4',
@@ -27,6 +35,4 @@ app.post('/', (request, response) => {
     message: 'DADOS SALVOS COM SUCESSO',
   })
 })
-
-// Pode usar um LISTEN para testar o retorno do servidor funcionando via npm run dev;
-app.listen(7777, () => console.log("rodando"));
+*/
